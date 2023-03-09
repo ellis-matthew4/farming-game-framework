0cfa0e6c7c4cc876a13cba2f2fc4fcd3aca2df77
Matthew Ellis <42591412+ellis-matthew4@users.noreply.github.com>
2023-03-09 12:08:24 -0600

    Improved quick inventory implementation and controls

--- src/scripts/quick_inventory_slot.gd
+++ src/scripts/quick_inventory_slot.gd
@@ -15,13 +15,17 @@ func _ready():
 func _process(delta):
 	if focused and Globals.can_accept_mw_input:
 		if Input.is_action_just_released("ui_mwu"):
-			get_node("../ColorRect" + str(_get_previous_index())).grab_focus()
-			Globals.can_accept_mw_input = false
-			_wait_and_reset()
+			get_node(focus_previous).grab_focus()
+			_wait_and_reset(0.01)
 		elif Input.is_action_just_released("ui_mwd"):
-			get_node("../ColorRect" + str(_get_next_index())).grab_focus()
-			Globals.can_accept_mw_input = false
-			_wait_and_reset()
+			get_node(focus_next).grab_focus()
+			_wait_and_reset(0.01)
+		elif Input.is_action_pressed("ux_left"):
+			get_node(focus_previous).grab_focus()
+			_wait_and_reset(0.125)
+		elif Input.is_action_pressed("ux_right"):
+			get_node(focus_next).grab_focus()
+			_wait_and_reset(0.125)
 	
 
 func _on_focus_entered():
@@ -32,18 +36,7 @@ func _on_focus_exit():
 	color = Color.from_string("#959595", Color.DARK_GRAY)
 	focused = false
 
-func _wait_and_reset():
-	await get_tree().create_timer(0.01).timeout
+func _wait_and_reset(timeout):
+	Globals.can_accept_mw_input = false
+	await get_tree().create_timer(timeout).timeout
 	Globals.can_accept_mw_input = true
-
-func _get_next_index():
-	if index == 10:
-		return 1
-	else:
-		return index + 1
-
-func _get_previous_index():
-	if index == 1:
-		return 10
-	else:
-		return index - 1
